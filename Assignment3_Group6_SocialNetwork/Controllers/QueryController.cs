031ab75e23b878b678a88799b2693946ce3e737f
Mikkel-KR <Mikkel.kora@gmail.com>
2020-05-01 15:39:23 +0200

    hejsa

---
+++ Assignment3_Group6_SocialNetwork/Controllers/QueryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment3_Group6_SocialNetwork.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Feed()
+        {
+            
+        }
+    }
+}
--- Assignment3_Group6_SocialNetwork/Models/User.cs
+++ Assignment3_Group6_SocialNetwork/Models/User.cs
@@ -17,16 +17,18 @@ namespace Assignment3_Group6_SocialNetwork.Models
         public string UserName { get; set; }
         public char Gender { get; set; }
         public int Age { get; set; }
-        public List<Circle> Circles { get; set; } // <-- 4. GetAllCircleId's()
+
+        // List of Circles
+        public List<Circle> Circles { get; set; }
 
         // List of blocked users
         public List<string> BlockedUserIds { get; set; }
 
         // List of users who's is being followed
-        public List<string> FollowingUserIds { get; set; } //<-- 2. id's // <-- 3. userCollection (get all where id's == userCollection.id's) // <-- 5. Post which is public or CircleId's == myCircleId's
+        public List<string> FollowingUserIds { get; set; } 
 
         // List of own Posts (in circles and public)
-        public List<Post> Posts { get; set; } //1. <- getAll
+        public List<Post> Posts { get; set; }
 
     }
 }
